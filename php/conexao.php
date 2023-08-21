fb9d77f75128e473ca85bfee3f153c12efdc03bf
fireretaw <danilofernando1998@gmail.com>
2023-08-21 20:33:23 -0300

    Criação da API

---
+++ php/conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+    //Variáveis de acesso
+    $url = "localhost"
+    $usuario = "root";
+    $senha = "";
+    $base = "api";
+
+    //Conexão
+    $conexao = mysqli_connect($url, $usuario, $senha, $base);
+?>
