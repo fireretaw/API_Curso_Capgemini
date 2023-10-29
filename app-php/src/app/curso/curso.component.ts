0ad0f26d4b5ba4094abc929d00eaebe44b125fae
fireretaw <danilofernando1998@gmail.com>
2023-10-28 21:50:45 -0300

    finalizado projeto

--- app-php/src/app/curso/curso.component.ts
+++ app-php/src/app/curso/curso.component.ts
@@ -8,44 +8,88 @@ import { CursoService } from './curso.service';
   templateUrl: './curso.component.html',
   styleUrls: ['./curso.component.css']
 })
-export class CursoComponent implements OnInit{
+export class CursoComponent implements OnInit {
 
 
   //Vetor de cursos
-  vetor:Curso[] = [];
+  vetor: Curso[] = [];
   //Objeto da classe curso
   curso = new Curso();
 
   //Construtor 
-  constructor(private curso_servico:CursoService){}
+  constructor(private curso_servico: CursoService) { }
 
-  
+
 
   //Inicializador
-  ngOnInit(){
+  ngOnInit() {
     //Ao iniciar o sistema, deverá listar os cursos
     this.selecao();
   }
 
 
   //Cadastro
-  cadastro(){
+  cadastro(curso:Curso) {
+
+    this.curso_servico.cadastrarCurso(this.curso).subscribe(
+      (res: Curso[]) => {
+        this.vetor = res;
+
+        //limpar atributos
+        this.curso.valorCurso = null;
+        this.curso.nomeCurso = null;
+
+      }
+    )
   }
 
   //Seleção
-  selecao(){
-    this.curso_servico.obterCursos().subscribe(
-      (res: Curso[]) =>{
+  selecao() {
+    this.curso_servico.obterCurso().subscribe(
+      (res: Curso[]) => {
         this.vetor = res;
       }
     )
-    
+
   }
 
-  //Alterar
-  alterar(){
-  }
   //Excluir
-  excluir(){
+  excluir(curso:Curso) {
+
+    this.curso_servico.removerCurso(this.curso).subscribe(
+      (res: Curso[]) => {
+        this.vetor = res;
+
+        //limpar atributos
+        this.curso.valorCurso = null;
+        this.curso.nomeCurso = null;
+      }
+    )
+  }
+
+  //selecionar Curso Específico
+
+  selecionarCurso(c: Curso) {
+    this.curso.idCurso = c.idCurso;
+    this.curso.valorCurso = c.valorCurso;
+    this.curso.nomeCurso = c.nomeCurso;
+
+  }
+
+
+  
+  //alterar
+  alterar() {
+
+    this.curso_servico.alterarCurso(this.curso).subscribe(
+      (res) => {
+        this.vetor = res;
+        
+        //limpar atributos
+        this.curso.valorCurso = null;
+        this.curso.nomeCurso = null;
+      }
+    )
   }
 }
+
--- app-php/src/app/curso/curso.service.ts
+++ app-php/src/app/curso/curso.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { map} from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { filter, map } from 'rxjs/operators';
 import { Curso } from './curso';
 import { Observable } from 'rxjs';
 
@@ -10,22 +10,63 @@ import { Observable } from 'rxjs';
 export class CursoService {
 
   //URL
-  url = "http://localhost/api/projeto-api/php/";
+  url = "http://localhost/api/php/";
 
   //Vetor
-  vetor:Curso[] = [];
+  vetor: Curso[] = [];
 
   //Construtor
   constructor(private http: HttpClient) { }
 
   //Obter todos os cursos
-  obterCursos():Observable<Curso[]>{
-    
-    return this.http.get(this.url+"listar").pipe(
-      map((res : any) => {
+  obterCurso(): Observable<Curso[]> {
+
+    return this.http.get(this.url + 'listar').pipe(
+      map((res) => {
         this.vetor = res['cursos'];
         return this.vetor;
       })
     )
+  }
+
+  //Cadastrar cursos
+  cadastrarCurso(c: Curso): Observable<Curso[]> {
+    return this.http.post(this.url + 'cadastrar', { cursos: c }).pipe(
+      map((res) => {
+        this.vetor.push(res['cursos']);
+        return this.vetor;
+      }))
+  }
+
+  //remover cursos
+  removerCurso(c: Curso): Observable<Curso[]> {
+
+    const params = new HttpParams().set("idCurso", c.idCurso.toString());
+    return this.http.delete(this.url + 'excluir', { params: params }).pipe(
+      map((res) => {
+        const filtro = this.vetor.filter((curso) => {
+          return + curso['idCurso'] !== +c.idCurso;
+        });
+
+      }));
+
+    return this.vetor = filtro;
+  }
+
+  //Atualizar cursos
+  alterarCurso(c: Curso): Observable<Curso[]> {
+    return this.http.put(this.url + 'alterar', { cursos: c }).pipe(
+      map((res) => {
+        const cursoAlterado = this.vetor.find((item) => {
+          return + item['idCurso'] === +[idCurso];
+        });
+
+        if(cursoAlterado){
+          cursoAlterado['nomeCurso'] = c['nomeCurso'];
+          cursoAlterado['valorCurso'] = c['valorCurso'];
+        }
+
+        return this.vetor;
+      }))
   }
 }
